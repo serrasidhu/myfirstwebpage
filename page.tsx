c018e69afcd22b2233e968eeb7fcd18d203587b2
Sara Murtaza <140093107+serrasidhu@users.noreply.github.com>
2023-11-19 20:17:04 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,106 @@
+import { Red_Hat_Display } from 'next/font/google'
+import Image from 'next/image'
+import { join } from 'path'
+
+export default function Home() {
+  return (
+    <main>
+      <header>
+      <title>Web Page</title>
+      <h1 >
+        <center>
+          <br></br>
+          Certified Web 3.0 and Metaverse Developer:
+        <br></br> A Nationwide Program in Karachi,Lahore,
+        <br></br> Islamabad, and Peshawar
+        </center> 
+      </h1>
+      <center><p>Getting Ready for the Next Generation and Future of the Internet - Join a 13 Trillion Dollar Industry with 5 Billion Users</p><br></br></center>
+      </header>
+      <h2>
+        <center>
+        Certified Web 3.0 and Metaverse Developer: A<br></br>Nationwide Program in Karachi, Lahore,<br></br>Islamabad, and Peshawar</center>
+         </h2>
+        <center><img src='https://www.panaverse.co/red-p-logo-text_dao_croped.png'></img></center>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/FwARACe8M_4?si=AaPWQ-MfQPtUdIvu" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+        <section>
+        <h3>
+          <center>
+          The Future of the Web is Web 3.0, Metaverse, and Edge Computing.<br></br> 
+          Panaverse DAO is a movement to spread these technolgies globally.<br></br> 
+          It is community of Web 3 and Metaverse developers, designers, trainers, <br></br>
+          startup founders and service providers.</center>
+          </h3>    
+       <center><a href='https://www.piaic.org/'>Admissions Now Open in Karachi, Lahore, Islamabad, and
+       <br></br> Peshawar <br></br>
+       
+         </a>
+         <a href='https://docs.google.com/document/d/13Z4tMKQmZpMEsOc1Y_qAXbOstRolGIFupHxQFgAFl98/edit?usp=sharing'>This document on Google Docs
+        </a>
+       
+       </center> 
+        
+      </section>
+        <img src="https://www.dar.org.pe/wp-content/uploads/2020/01/Logotipo-PIACI_aprobado-2.jpg" width={1100} height={600}/>
+          <center>
+            <p>
+          The internet is without a doubt the most important technological development in human history.<br></br>
+           Web3 and metaverse technologies expand the internet as we know it by introducing novel features<br></br>
+           and advancements. Metaverse will make use of all aspects of modern technology, including 3D, VR,<br></br>
+            AR, AI, blockchain, cloud computing, voice computing, ambient computing, and more.<br></br>
+            
+            Citi is the latest Wall Street business to give a positive prognosis for Web 3.0 and the Metaverse,<br></br> 
+            terms used to depict a future internet vision centred on decentralised technologies and virtual<br></br> 
+            worlds. Citi stated in a March 2022 research paper that the metaverse economy might have a total <br></br>
+            addressable market of up to $13 trillion and five billion people by 2030.
+            </p>
+            </center>
+            <h4>
+              <center>The Program in a Nutshell: Earn While You Learn</center></h4> 
+            <center>  <p>In this brand-new type of curriculum, students will learn how to make money and boost exports in <br></br>the classroom and will begin doing so within six months of the program’s beginning. It resembles a <br></br>cross between a corporate venture and an educational project.</p>           
+              </center>
+
+                <center>
+                
+<table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+  <tr>
+    <td>Ernst Handel</td>
+    <td>Roland Mendel</td>
+    <td>Austria</td>
+  </tr>
+  <tr>
+    <td>Island Trading</td>
+    <td>Helen Bennett</td>
+    <td>UK</td>
+  </tr>
+  <tr>
+    <td>Laughing Bacchus Winecellars</td>
+    <td>Yoshi Tannamuri</td>
+    <td>Canada</td>
+  </tr>
+  <tr>
+    <td>Magazzini Alimentari Riuniti</td>
+    <td>Giovanni Rovelli</td>
+    <td>Italy</td>
+  </tr>
+</table>
+</center>
+              </main>
+ 
+  )
+}
